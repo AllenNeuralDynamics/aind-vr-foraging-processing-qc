a5d6a681036753e740b15f358497169d922c25d2
arjunsridhar12345 <109561860+arjunsridhar12345@users.noreply.github.com>
2025-08-14 16:47:58 -0700

    Processed qc (#1)
    * initial qc
    
    * feat: add additional qc
    
    * fix linting
    
    * lint: isort on utils
    
    * fix run script
    
    * fix: add title to licks distribution plot
    
    * combine raw and processed qc
    
    * fix: update name of timeseries for running metric
    
    * refactor: modify licks qc metrics
    
    * fix: only computer inter-lick interval if possible
    
    * docs: add initial readme
    
    * fix: automate passing and failing of metrics and add rule to description
    
    * remove qc-portal schema dependecy
    
    * refactor: move thresholds to settings model and simplify metrics descriptions
    
    * fix linting

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,7 +1,159 @@
-""" top level run script """
+import json
+import logging
+from pathlib import Path
 
-def run():
-    """ basic run function """
-    pass
+from aind_data_schema.core.quality_control import (QCEvaluation, QCMetric,
+                                                   QualityControl, Stage)
+from aind_data_schema_models.modalities import Modality
+from hdmf_zarr import NWBZarrIO
+from pydantic import Field
+from pydantic_settings import BaseSettings
 
-if __name__ == "__main__": run()
+import utils
+
+logger = logging.getLogger(__name__)
+
+
+class VRForagingSettings(BaseSettings, cli_parse_args=True):
+    """
+    Settings for VR Foraging Primary Data NWB Packaging
+    """
+
+    input_directory: Path = Field(
+        default=Path("/data/vr_foraging_processed_nwb"),
+        description="Directory where data is",
+    )
+    output_directory: Path = Field(
+        default=Path("/results/"), description="Output directory"
+    )
+    environment_threshold_low: float = Field(
+        default=23,
+        description=str(
+            "Threshold to check if metric is "
+            "lower than this, than fails",
+        )
+    )
+    environment_threshold_high: float = Field(
+        default=28,
+        description=str(
+            "Threshold to check if metric is "
+            "higher than this, than fails",
+        )
+    )
+    velocity_threshold: float = Field(
+        default=70,
+        description=str(
+            "Threshold to check if metric is "
+            "higher than this, than fails",
+        )
+    )
+    lick_density_threshold: float = Field(
+        default=0.1,
+        description=str(
+            "Threshold to check if metric is "
+            "lower than this, than fails",
+        )
+    )
+    number_of_licks_threshold: float = Field(
+        default=1000,
+        description=str(
+            "Threshold to check if metric is "
+            "higher than this, than fails",
+        )
+    )
+
+
+def get_qc_evaluation(name: str, metric: QCMetric) -> QCEvaluation:
+    """
+    Gets the qc evaluation
+
+    Parameters
+    ----------
+    name: str
+        The name of the evaluation
+
+    metric: QCMetric
+        The qc metric
+
+    Returns
+    -------
+    QCEvaluation
+        The qc evalution with the qc metric
+    """
+    evaluation = QCEvaluation(
+        modality=Modality.BEHAVIOR,
+        stage=Stage.PROCESSING,
+        name=name,
+        description=name,
+        metrics=metric,
+    )
+
+    return evaluation
+
+
+if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+    settings = VRForagingSettings()
+    logger.info(f"Settings: {settings}")
+    paths = tuple(settings.input_directory.glob("*"))
+    processed_nwb_path = [path for path in paths if path.is_dir()]
+
+    if not processed_nwb_path:
+        raise FileNotFoundError("No processed nwb found")
+
+    logger.info(f"Found processed nwb at path {processed_nwb_path[0]}")
+    with NWBZarrIO(processed_nwb_path[0].as_posix(), "r") as io:
+        nwb = io.read()
+
+    logger.info("Computing environment condition evaluation")
+    environment_metrics = utils.get_environment_qc_metrics(
+        nwb,
+        settings.output_directory,
+        settings.environment_threshold_low,
+        settings.environment_threshold_high,
+    )
+
+    logger.info("Computing running velocity evaluation")
+    running_velocity_metric = utils.get_running_velocity_qc_metric(
+        nwb, settings.output_directory, settings.velocity_threshold
+    )
+
+    logger.info("Computing general peformance evaluation")
+    general_performance_metrics = utils.get_general_performance_qc_metrics(
+        nwb, settings.output_directory
+    )
+
+    logger.info("Computing lick evaluations")
+    lick_metrics = utils.get_lick_qc_metrics(
+        nwb,
+        settings.output_directory,
+        settings.lick_density_threshold,
+        settings.number_of_licks_threshold,
+    )
+
+    evaluations = []
+
+    for name, metrics in environment_metrics.items():
+        evaluations.append(get_qc_evaluation(name, metrics))
+
+    for name, metrics in running_velocity_metric.items():
+        evaluations.append(get_qc_evaluation(name, metrics))
+
+    for name, metrics in general_performance_metrics.items():
+        evaluations.append(get_qc_evaluation(name, metrics))
+
+    for name, metrics in lick_metrics.items():
+        evaluations.append(get_qc_evaluation(name, metrics))
+
+    with open(settings.input_directory.parent / "raw_qc.json", "r") as f:
+        raw_qc = json.load(f)
+
+    for evaluation in QualityControl(**raw_qc).evaluations:
+        evaluations.append(evaluation)
+
+    quality_control = QualityControl(evaluations=evaluations)
+    with open(settings.output_directory / "quality_control.json", "w") as f:
+        f.write(quality_control.model_dump_json(indent=4))
---
+++ code/utils.py
@@ -0,0 +1,309 @@
+import json
+from datetime import datetime
+from pathlib import Path
+from typing import Dict, List
+
+import matplotlib.pyplot as plt
+import numpy as np
+from aind_data_schema.core.quality_control import QCMetric, QCStatus, Status
+from pynwb import NWBFile
+from scipy.stats import gaussian_kde
+
+status_pending = QCStatus(
+    evaluator="", status=Status.PENDING, timestamp=datetime.now()
+)
+
+status_pass = QCStatus(
+    evaluator="", status=Status.PASS, timestamp=datetime.now()
+)
+
+status_fail = QCStatus(
+    evaluator="", status=Status.FAIL, timestamp=datetime.now()
+)
+
+
+def get_environment_qc_metrics(
+    nwb: NWBFile,
+    output_path: Path,
+    low_threshold: float,
+    high_threshold: float,
+) -> Dict[str, List[QCMetric]]:
+    """
+    Gets qc metrics for envrionmental conditions
+
+    Parameters
+    ----------
+    nwb: NWBFile
+        The processed nwb file
+
+    output_path: Path
+        Output directory where figures are to be saved
+
+    low_threshold: float
+        The threshold to check if values are higher than this
+
+    high_threshold: float
+        The threshold to check if values are lower than this
+
+    Returns
+    -------
+    Dict[str, List[QCMetric]]
+        Dictionary with qc metrics
+    """
+    qc_metrics = {"Enviornmental Conditions": []}
+    sensor_data = nwb.acquisition["Behavior.HarpEnvironmentSensor.SensorData"][
+        :
+    ]
+
+    for metric_name in ("Temperature", "Humidity"):
+        average = float(sensor_data[metric_name].mean())
+        if average < low_threshold or average > high_threshold:
+            status = status_fail
+        else:
+            status = status_pass
+
+        fig, ax = plt.subplots()
+        ax.plot(sensor_data["Time"], sensor_data[metric_name])
+        ax.set_xlabel("Time (s)")
+        ax.set_ylabel(metric_name)
+        ax.set_title(f"Environment - {metric_name}")
+
+        fig.savefig(output_path / f"environment_{metric_name}.png")
+        qc_metric = QCMetric(
+            name=f"Environment - {metric_name}",
+            value={"Average": average},
+            reference=f"environment_{metric_name}.png",
+            description=str(
+                f"Fail when Average {metric_name} is below "
+                f"{low_threshold} or is above "
+                f"{high_threshold}."
+            ),
+            status_history=[status],
+        )
+        qc_metrics["Enviornmental Conditions"].append(qc_metric)
+
+    return qc_metrics
+
+
+def get_running_velocity_qc_metric(
+    nwb: NWBFile, output_path: Path, threshold: float
+) -> Dict[str, List[QCMetric]]:
+    """
+    Gets the running velocity from the processed nwb
+
+    Parameters
+    ----------
+    nwb: NWBFile
+        The processed nwb file
+
+    output_path: Path
+        Output directory where figures are to be saved
+
+    threshold: float
+        The threshold to check if values are lower than this
+
+    Returns
+    -------
+    Dict[str, List[QCMetric]]
+        Dictionary with qc metrics
+    """
+    metric_name = "Running Velocity"
+    running_data = nwb.processing["behavior"].data_interfaces["Encoder"]
+    data = running_data.data[:]
+    velocity_average = float(np.nanmean(data))
+    if velocity_average < 0 or velocity_average > threshold:
+        status = status_fail
+    else:
+        status = status_pass
+
+    qc_metric = QCMetric(
+        name=metric_name,
+        value={"Average": velocity_average},
+        description=str(
+            f"Fail when Average {metric_name} is "
+            f"below 0 or above {threshold}"
+        ),
+        status_history=[status],
+    )
+
+    return {metric_name: [qc_metric]}
+
+
+def get_general_performance_qc_metrics(
+    nwb: NWBFile, output_path: Path
+) -> Dict[str, List[QCMetric]]:
+    """
+    Gets the general performance metrics from the processed nwb
+
+    Parameters
+    ----------
+    nwb: NWBFile
+        The processed nwb file
+
+    output_path: Path
+        Output directory where figures are to be saved
+
+    Returns
+    -------
+    Dict[str, List[QCMetric]]
+        Dictionary with qc metrics
+    """
+    metric_name = "General Performance"
+    events = nwb.get_events__events_tables().to_dataframe()
+
+    total_rewards = len(events[events["event_name"] == "GiveReward"])
+    total_patches = (
+        events[events["event_name"] == "ActivePatch"]["event_data"]
+        .unique()
+        .size
+    )
+
+    if total_patches == 0 or total_rewards == 0:
+        status = status_fail
+    else:
+        status = status_pass
+
+    qc_metric = QCMetric(
+        name=metric_name,
+        value={
+            "Total Rewards": total_rewards,
+            "Total Patches": total_patches,
+        },
+        description=str(
+            f"Fail {metric_name}: When any metrics have value 0.",
+        ),
+        status_history=[status],
+    )
+
+    return {metric_name: [qc_metric]}
+
+
+def get_lick_qc_metrics(
+    nwb: NWBFile,
+    output_path: Path,
+    density_threshold: float,
+    number_of_licks_threshold: float,
+) -> Dict[str, List[QCMetric]]:
+    """
+    Gets the lick metrics from the processed nwb
+
+    Parameters
+    ----------
+    nwb: NWBFile
+        The processed nwb file
+
+    output_path: Path
+        Output directory where figures are to be saved
+
+    density_threshold: float
+        The threshold to check if the lick density are lower than this
+
+    number_of_licks_threshold: float
+        The threshold to check if number of licks are higher than this
+
+    Returns
+    -------
+    Dict[str, List[QCMetric]]
+        Dictionary with qc metrics
+    """
+    metric_name = "Licks"
+    metrics = {metric_name: []}
+
+    events = nwb.get_events__events_tables().to_dataframe()
+    events["event_data"] = events["event_data"].apply(json.loads)
+    licks = events[events["event_name"] == "Lick"]
+    licks["event_data"] = licks["event_data"].astype(bool)
+
+    fig, ax = plt.subplots()
+    inter_licks_distribution = np.diff(licks[licks["event_data"]]["timestamp"])
+    # KDE
+    if len(inter_licks_distribution) > 1:
+        kde = gaussian_kde(inter_licks_distribution)
+        x = np.linspace(0, max(inter_licks_distribution), 500)
+        density = kde(x)
+
+        below_mask = density < density_threshold
+        count_below = np.sum(below_mask)
+        proportion_below = count_below / len(density)
+
+        if proportion_below < density_threshold:
+            status = status_fail
+        else:
+            status = status_pass
+
+        ax.plot(x, density)
+        ax.axhline(0.1, color="r", linestyle="--")
+        ax.set_xlabel("Inter-lick interval (s)")
+        ax.set_ylabel("Density")
+        ax.set_title("Inter-licks Distribution")
+        fig.savefig(output_path / "inter_licks_distribution.png")
+        qc_metric_inter_licks_distribution = QCMetric(
+            name="Inter-licks distribution",
+            value=None,
+            reference="inter_licks_distribution.png",
+            description=str(f"Fail when density < " f"{density_threshold}."),
+            status_history=[status],
+        )
+        metrics[metric_name].append(qc_metric_inter_licks_distribution)
+
+    number_of_licks = len(licks[licks["event_data"]])
+    if number_of_licks == 0 or number_of_licks > number_of_licks_threshold:
+        status = status_fail
+    else:
+        status = status_pass
+
+    qc_metric_lick_count = QCMetric(
+        name="Number of licks",
+        value={"Number of licks": number_of_licks},
+        description=str(
+            "Fail when number of licks is 0 or "
+            f"above {number_of_licks_threshold}."
+        ),
+        status_history=[status],
+    )
+    metrics[metric_name].append(qc_metric_lick_count)
+
+    # offset - onset
+    lick_onset_offset = nwb.acquisition["Behavior.HarpLickometer.LickState"][:]
+    onsets = lick_onset_offset.loc[
+        lick_onset_offset["Channel0"], "Time"
+    ].reset_index(drop=True)
+    offsets = lick_onset_offset.loc[
+        ~lick_onset_offset["Channel0"], "Time"
+    ].reset_index(drop=True)
+
+    durations = onsets - offsets
+    lick_variability_within = np.nanstd(durations)
+
+    # Plot histogram
+    fig, ax = plt.subplots()
+    ax.hist(durations, bins=30, alpha=0.7, edgecolor="black")
+
+    # Mark mean
+    mean_dur = np.mean(durations)
+    ax.axvline(
+        mean_dur, color="blue", linestyle="--", label=f"Mean = {mean_dur:.3f}s"
+    )
+
+    ax.set_xlabel("Lick duration (s)")
+    ax.set_ylabel("Count")
+    ax.set_title("Within-lick Duration Distribution (Onset - Offset)")
+    ax.set_xlim((0, 200))
+    ax.legend()
+    fig.savefig(output_path / "licks.png")
+
+    # TODO: get threshold for failing if larger
+    qc_metric_within_lick_distribution = QCMetric(
+        name="Within lick variability (onset - offset)",
+        value={
+            "Within lick variability (onset - offset)": float(
+                lick_variability_within
+            )
+        },
+        reference="licks.png",
+        description="Within lick variability",
+        status_history=[status_pending],
+    )
+    metrics[metric_name].append(qc_metric_within_lick_distribution)
+
+    return metrics
